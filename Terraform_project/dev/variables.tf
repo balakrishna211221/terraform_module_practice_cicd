466a8a23b2361152306fbc6effb052296f7bf773
balakrishna211221 <37782387+balakrishna211221@users.noreply.github.com>
2024-07-10 19:15:42 +0530

    Update variables.tf

--- Terraform_project/dev/variables.tf
+++ Terraform_project/dev/variables.tf
@@ -1,16 +1,19 @@
 variable "aws_access_key" {
   type      = string
   sensitive = true
+ default = ""
 }
 
 variable "aws_secret_key" {
   type      = string
   sensitive = true
+ default = ""
 }
 
 variable "secret_name" {
   type        = string
   description = "The name of the secret to retrieve from AWS Secrets Manager"
+ default = ""
 }
 
 
